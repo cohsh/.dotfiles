151583bea13834e111dca2c6ca39efd5e619e23f
ko-ishii <82075327+ko-ishii@users.noreply.github.com>
2022-11-27 16:04:09 +0900

    change: color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = {
 	font = wezterm.font('Source Code Pro'),
 	font_size = 18.0,
 	use_ime = true,
-	color_scheme = 'Dracula',
+	color_scheme = 'Orangish (terminal.sexy)',
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
     window_background_image = os.getenv('HOME') .. '/.config/wezterm/background/main.png',
     window_background_image_hsb = { brightness =0.01, hue = 1.0, saturation = 1.0 }
