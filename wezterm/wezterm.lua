614c16ef47d971a65de546862fa97cb60b0bd451
cohsh <82075327+cohsh@users.noreply.github.com>
2022-12-11 13:11:57 +0900

    [change] Color Scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require('wezterm')
 
 local config = {
 	font = wezterm.font('Source Code Pro'),
-	font_size = 16.0,
+	font_size = 18.0,
 	use_ime = true,
-	color_scheme = 'Orangish (terminal.sexy)',
+	color_scheme = 'pinky (base16)',
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
     window_background_image = os.getenv('HOME') .. '/.config/wezterm/background/main.png',
     window_background_image_hsb = { brightness =0.005, hue = 1.0, saturation = 1.0 }
