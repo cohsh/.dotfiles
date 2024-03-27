6f3e2b42b3c120dbc82007b4ae543ee151d11034
cohsh <82075327+cohsh@users.noreply.github.com>
2024-03-27 11:07:15 +0900

    :zap: use an official wallpaper

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,8 @@ local config = {
 	use_ime = true,
 	color_scheme = 'pinky (base16)',
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-    window_background_image = os.getenv('HOME') .. '/.config/wezterm/background/main.png',
-    window_background_image_hsb = { brightness =0.005, hue = 1.0, saturation = 1.0 }
+    window_background_image = '/usr/share/backgrounds/archlinux/small.png',
+    window_background_image_hsb = { brightness =0.5, hue = 1.0, saturation = 1.0 }
 }
 
 return config
