018ec9ca1fa9f955a1105d5aabcef567e846a825
cohsh <82075327+cohsh@users.noreply.github.com>
2024-11-02 19:48:54 +0900

    :zap: added lines about pyenv

--- zsh/zshrc.d/common/mizc.zsh
+++ zsh/zshrc.d/common/mizc.zsh
@@ -1,2 +1,4 @@
 export EDITOR=nvim
 export LESS='--tabs=4 --no-init --LONG-PROMPT --ignore-case -R'
+
+eval "$(pyenv init --path)"
--- zsh/zshrc.d/common/path.zsh
+++ zsh/zshrc.d/common/path.zsh
@@ -1,2 +1,3 @@
-# For Mac
-export PATH=$HOME/.nodebrew/current/bin:$HOME/.local/bin:$PATH
+export PYENV_ROOT=$HOME/.pyenv
+
+export PATH=$HOME/.nodebrew/current/bin:$HOME/.local/bin:$PYENV_ROOT/bin:$PATH
