3c221e5f20a4b4e17b49bf099307eef39e714452
cohsh <82075327+cohsh@users.noreply.github.com>
2026-07-16 11:08:44 +0900

    :zap: wezterm: added check for background image

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,14 @@ local config = {
 	use_ime = true,
 	color_scheme = 'pinky (base16)',
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-    window_background_image = '/usr/share/backgrounds/archlinux/small.png',
-    window_background_image_hsb = { brightness =0.5, hue = 1.0, saturation = 1.0 }
 }
+
+local background = '/usr/share/backgrounds/archlinux/small.png'
+local f = io.open(background, 'r')
+if f then
+	f:close()
+	config.window_background_image = background
+	config.window_background_image_hsb = { brightness = 0.5, hue = 1.0, saturation = 1.0 }
+end
 
 return config
