f4b601df843bd7d37f775371c8ddebcd442967a7
cohsh <82075327+cohsh@users.noreply.github.com>
2026-07-16 11:08:44 +0900

    :zap: zsh: mizc.zsh: added guards for pyenv and pager

--- zsh/zshrc.d/common/mizc.zsh
+++ zsh/zshrc.d/common/mizc.zsh
@@ -1,4 +1,7 @@
 export EDITOR=nvim
 export LESS='--tabs=4 --no-init --LONG-PROMPT --ignore-case -R'
 
-eval "$(pyenv init --path)"
+command -v pyenv > /dev/null && eval "$(pyenv init --path)"
+
+## pager for non-TTY output
+[[ ! -t 1 ]] && export PAGER=cat GIT_PAGER=cat
